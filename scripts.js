bf74e027ff04fdfc94d4efc11927fa04dda6e021
Bethan <bethancdavies@gmail.com>
2020-01-20 11:33:36 -0500

    updates to keydown variables

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
 startButton = document.querySelector('.startButton');
-homeScreen = document.querySelector('.homeScreen') 
+homeScreen = document.querySelector('.homeScreen');
+leftArrow = document.querySelector('#left');
+upArrow = document.querySelector('#up');
+rightArrow = document.querySelector('#right'); 
+downArrow = document.querySelector('#down');
+
+
+
 
 startButton.addEventListener("click", hidden) 
 // removes text from page before Starting game
 function hidden (){ 
     homeScreen.classList.add('hidden') 
-}
+} 
+
 
 // write basic keydown activities 
 // left up right down 
