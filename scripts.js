63d87f601d2c53d15438c63a54f00e86b530190f
Bethan <bethancdavies@gmail.com>
2020-01-20 09:28:26 -0500

    wireframe in html completed, psuedo code written for functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 startButton = document.querySelector('.startButton');
+homeScreen = document.querySelector('.homeScreen') 
 
-startButton.addEventListener("click", function(){
-    console.log('ive been clicked') 
-    // function to start game
-})
+startButton.addEventListener("click", hidden) 
+// removes text from page before Starting game
+function hidden (){ 
+    homeScreen.classList.add('hidden') 
+}
+
+// write basic keydown activities 
+// left up right down 
+
+// to do: pause button, mute button
+// score counter 
+// functionality, how do I want it to work 
+// animate and generate falling buttons? icons easiest?
